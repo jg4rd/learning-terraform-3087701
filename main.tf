d3ce6d9e01cb4c38a501226bdf0e857de5e15b01
jg4rd <no@exmaple.com>
2024-03-28 10:50:12 -0400

    oopsies!

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "blog" {
 
 resource "aws_security_group_rule" "blog_http_in"{
   type = "ingress"
-  from_port - 80
+  from_port = 80
   to_port = 80
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
@@ -48,7 +48,7 @@ resource "aws_security_group_rule" "blog_http_in"{
 
 resource "aws_security_group_rule" "blog_https_in"{
   type = "ingress"
-  from_port - 443
+  from_port = 443
   to_port = 443
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
@@ -58,7 +58,7 @@ resource "aws_security_group_rule" "blog_https_in"{
 
 resource "aws_security_group_rule" "blog_everything_out"{
   type = "egress"
-  from_port - 0
+  from_port = 0
   to_port = 0
   protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
